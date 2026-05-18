be4e74e7f52f6fe5112f27bbb7351823327a14af
AkimioJR <akimio.jr@gmail.com>
2026-05-18 23:59:00 +0800

    update: add minimum platform support restrictions

--- Package.swift
+++ Package.swift
@@ -5,12 +5,15 @@ import PackageDescription
 
 let package = Package(
     name: "BrewKit",
+    platforms: [
+        .macOS(.v13)
+    ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "BrewKit",
             targets: ["BrewKit"]
-        ),
+        )
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
